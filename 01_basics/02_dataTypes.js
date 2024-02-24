c954ccaf5c8b4bd91bc7c3eef2a00a81aa28f922
anshsingh7755 <121092055+anshsingh7755@users.noreply.github.com>
2024-02-24 08:52:49 +0000

    data types

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,1 +1,33 @@
-"use strict" //treat all js code as newer version
+"use strict"; //treat all js code as newer version
+
+//alert( 3 +3 ) // we are using nodejs not browser
+
+console.log(3 +
+        3) // code readibility should be high
+
+console.log("Ansh")
+
+let name = "Ansh"
+
+let age = 21
+
+let isLoggedIn = false
+
+let state;
+
+// number => 2 to the power 53
+// bigint
+//string
+//boolean=> true/false
+//null => standalone value
+//undefined =>
+//Symbol=> unique
+
+//object
+console.log(typeof "Ansh")
+
+console.log(typeof age);
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // O/P UNDEFINED
