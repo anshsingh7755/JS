94c715f89e52e694feab516f10c815b459e23761
anshsingh7755 <121092055+anshsingh7755@users.noreply.github.com>
2024-03-01 14:14:38 +0000

    update commit

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -3,15 +3,15 @@ let score = "Ansh"
 //console.log(typeof score);
 //console.log(typeof(score));
 let valueInNumber = Number(score)
-//console.log(valueInNumber);
-//"33" =>33
-//"33abc " =>NaN
-//true =>1;false=>0 
+    //console.log(valueInNumber);
+    //"33" =>33
+    //"33abc " =>NaN
+    //true =>1;false=>0 
 
 let isLoggedIn = "Ansh"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn);
+    //console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -20,13 +20,13 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-//console.log(stringNumber);
-//console.log(typeof stringNumber);
+    //console.log(stringNumber);
+    //console.log(typeof stringNumber);
 
 /*--------------- Operations --------------*/
 let value = 3
 let negValue = -value
-//console.log(negValue);
+    //console.log(negValue);
 
 //console.log(2+2);
 //console.log(2-2);
@@ -39,12 +39,12 @@ let str1 = "hello"
 let str2 = " ansh"
 
 let str3 = str1 + str2
-//console.log(str3);
-//console.log("1" + 2);
-//console.log(1 + "2");
-//console.log("1" + "2");
-//console.log("1" + 2 + 2);
-//console.log(1 + 2 + "3");
+    //console.log(str3);
+    //console.log("1" + 2);
+    //console.log(1 + "2");
+    //console.log("1" + "2");
+    //console.log("1" + 2 + 2);
+    //console.log(1 + 2 + "3");
 
 //console.log((3 + 4)* 5%3);
 
@@ -52,11 +52,11 @@ let str3 = str1 + str2
 
 let num1, num2, num3
 
-num1 = num2 = num3 = 2+ 2
+num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
-++gameCounter;
+    ++gameCounter;
 console.log(gameCounter);
 
 //LINK TO STUDY
-// 
+//
