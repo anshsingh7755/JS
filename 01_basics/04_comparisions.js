3c34f905ede9bd5db12436d7cc539cce1c9df039
anshsingh7755 <121092055+anshsingh7755@users.noreply.github.com>
2024-03-01 14:40:25 +0000

    update commit

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,15 @@
+// console.log(2 < 3);
+//console.log(2 <= 3);
+//console.log(2 < 1);
+//console.log(2 == 3);
+//console.log(2 != 3);
+
+//console.log(null >= 0);
+
+//console.log(undefined == 0);
+
+// === check the values as values as well as the datatypes
+
+console.log("2" === 2);
+
+/*comparisons make us get confused*/
