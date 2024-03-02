e082d45705fcbfc7bfe8ce856aa78082bb46405b
anshsingh7755 <121092055+anshsingh7755@users.noreply.github.com>
2024-03-02 06:49:53 +0000

    commit

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -33,4 +33,40 @@ const
         console.log("Hello world")
     }
 
-console.log(typeof myFunction);
+//console.log(typeof myFunction);
+
+
+//***********************************//
+
+// stack(Primitive), Heap(Non-primitive)
+//when stack memory is used we get a copy of the variable used called
+//we get the reference of the original value(change in reference = change in org value)
+
+let myAccount = "Anshsingh7755"
+
+let myIgAccount = myAccount
+
+myIgAccount = "Ansh7755"
+
+console.log(myAccount);
+console.log(myIgAccount);
+
+let userOne = {
+    email:"user@gmail.com",
+    upi :"user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+/*notes
+all primitive values go in stack and when we 
+take anyhig from stack we get a copy of that \
+particular object rather getting original when 
+we keep anything in heap we take reference
+(if we change anything it actuallly applies on 
+original value)*/
