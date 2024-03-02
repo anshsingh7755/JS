4118519c9547bcded6e5c28a537154f0d97bdf53
anshsingh7755 <121092055+anshsingh7755@users.noreply.github.com>
2024-03-02 07:23:46 +0000

    commit

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,36 @@
+const name = "Ansh"
+const repoCount = 50
+
+//console.log(name + repoCount + "Value");
+
+//**//backtext - string interpolation
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('Ansh-singh-thakur')
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+ //console.log(gameName.indexOf('s'));
+
+const newString = gameName.substring(0,2) 
+//console.log(newString);
+
+const anotherString = gameName.slice(-6,3)
+//console.log(anotherString)
+
+const newStringOne = "    ansh     "
+//console.log(newStringOne);
+//console.log(newStringOne.trim());
+
+const url = "https://ansh.com/ansh%23singh"
+
+//console.log(url.replace('%23', '-'))
+
+//console.log(url.includes('Ansh'))
+
+console.log(gameName.split('-'));
+
+
