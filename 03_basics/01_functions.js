c00beae36c62b9b131f3ab20432d9b3aca9ca0d0
anshsingh7755 <121092055+anshsingh7755@users.noreply.github.com>
2024-03-05 18:30:15 +0000

    commit

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -2,16 +2,16 @@
 
 
 
-function sayMyName(){
-console.log("H");
-console.log("I");
-console.log("M");
-console.log("A");
-console.log("N");
-console.log("S");
-console.log("H");
-console.log("U");
-}
+// function sayMyName(){
+// console.log("H");
+// console.log("I");
+// console.log("M");
+// console.log("A");
+// console.log("N");
+// console.log("S");
+// console.log("H");
+// console.log("U");
+// }
 //  sayMyName()
 
 function addTwoNumbers(num1, num2){  //here num1,num2 are parameters(when we create definition of function and the input entered are parameters) and 3 and a are arguments when fnx are called
@@ -26,13 +26,13 @@ const result  = addTwoNumbers(3,5)
 
 function loginUserMessage(username){
     if(username == undefined){
-        console.log("Please enter a username");
+        // console.log("Please enter a username");
         return
     }
     return `${username} just loggeed in`
 }
 
- console.log(loginUserMessage())
+//  console.log(loginUserMessage())
 
  // we somwtimes also use !-(!username) sign to change the value in the place of username == undefined 
 
@@ -40,9 +40,36 @@ function loginUserMessage(username){
 // we can also define during creation of function to default assign the value
 // when any value is not given it assings default value and when  value is given it overwrites
 
+function calculateCartPrice (val1,val2,...num1){
+    return num1
+}
 
+// console.log(calculateCartPrice(200,400,500));
 
+const user = {
+    username:"Ansh",
+    prices: 100
+}
 
+function handleObject(anyobject){
+    // console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+handleObject({
+    username:"sam",
+    price:494
+})
+
+const myNewArray = [2200,400,400]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,300,4000]));
 
 
 
