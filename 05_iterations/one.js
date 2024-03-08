0f9a80c80945e9b17bc95c88058eb6ec794e2714
anshsingh7755 <121092055+anshsingh7755@users.noreply.github.com>
2024-03-08 07:18:46 +0000

    commit

---
+++ 05_iterations/one.js
@@ -0,0 +1,63 @@
+//for
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if (element == 5 ){
+
+        // console.log("5 is best no.");
+    }
+    // console.log(element);
+}
+// console.log(element);
+
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`outer loop value: ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`Inner loop value ${j} and inner loop${i}`);  
+        // console.log(i + '*' + j + '=' + i*j); 
+    }
+}
+
+
+let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+}
+
+
+// break and continue
+
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        // console.log(`detected 5`);
+        break         //it stops the flow completely
+    }
+    // console.log(`value of i is ${index}`);
+    
+}
+
+//continue
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`detected 5`);
+        continue    //it skips the condition by one step
+    console.log(`value of i is ${index}`);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
