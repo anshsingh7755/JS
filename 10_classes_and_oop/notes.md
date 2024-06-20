04198abdc464d61ab3e6f1204e5afce8deb0f456
anshsingh7755 <121092055+anshsingh7755@users.noreply.github.com>
2024-06-20 15:21:48 +0000

    commit

---
+++ 10_classes_and_oop/notes.md
@@ -0,0 +1,24 @@
+# javascript and classes
+
+## oop
+
+##object
+-collection of properties and methods
+-tolowercase(ex)
+
+##why use OOP
+
+##parrts of OOP
+Object Literal(literally that)
+
+-Counstructor function
+-Prototypes
+-Classes
+-Instances (new,this)
+
+## 4 Pillars
+Abstraction(hide details-fetch)
+Encapsulation(wrap up the data-)
+Inheritance()
+Polymorphism(functon used many times)
+
--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -4,9 +4,31 @@ const user = {
     signedIn: true,
 
     getUserDetails: function(){
-        console.log("got user details");
+        // console.log(`username: ${this.username}`);
+        console.log(this);
     }
 }
 
 console.log(user.username);
-console.log(user.getUserDetails);
+// console.log(user.getUserDetails());
+
+// console.log(this);
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    return this
+}
+
+const  userOne = User("ansh", 10 , true)
+
+console.log(userOne);
+
+
+
+
+
+
+
